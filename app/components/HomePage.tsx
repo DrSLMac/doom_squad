6756f4184b786ae6f322b50897cc79cc6d21ff4d
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-06-03 16:15:16 -0500

    added transition animation to logo

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import doomLogo from "../../public/doom-logo.png";
 import Image from "next/image";
+import { motion } from 'framer-motion'
 
 const HomePage = () => {
   return (
     <section>
-      <div className="flex justify-center mt-20">
+      <motion.div 
+        initial={{
+          x:0,
+          opacity: 0,
+          scale: 0.2
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1.1
+        }}
+        transition={{
+          type:"spring",
+          stiffness: 200,
+          duration: 1.5
+        }}
+        className="flex justify-center mt-20
+      ">
         <Image 
           src={doomLogo} 
           alt="Doom Squad Team Logo" 
           className="h-52 w-auto"
           priority
         />
-      </div>
+      </motion.div>
     </section>
   );
 };
