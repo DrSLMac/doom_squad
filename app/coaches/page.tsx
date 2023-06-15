e9dbbc62410af7736183d7c7602195c4edb26ea0
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2023-06-14 22:09:46 -0500

    finished mobile layout for coach page

--- app/coaches/page.tsx
+++ app/coaches/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import BallIcon from "../components/BallIcon";
 import { HomeIcon } from "@heroicons/react/24/solid";
+import Footer from "../components/Footer";
 
 export default async function Coaches() {
   const coaches = await getCoaches();
@@ -23,7 +24,8 @@ export default async function Coaches() {
             sm:px-16
             md:px-20
             lg:px-24 lg:pt-8
-      ">
+      "
+      >
         <BallIcon />
         <a href="/">
           <HomeIcon className="text-white short:h-8 h-10" />
@@ -31,40 +33,29 @@ export default async function Coaches() {
       </div>
       <h3 className="uppercase text-center mt-4 text-3xl">Coaching Staff</h3>
 
-      <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+      <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 pb-10">
         {sortedCoaches.map((coach) => (
           <Link
             href={`/coaches/${coach.slug}`}
             key={coach._id}
-            className="flex flex-col items-center border-2 border-gray-500 w-96 p-1 mx-auto rounded-lg mt-4 hover:scale-105 hover:border-neon transition"
+            className="flex flex-col items-center border-2 bg-gray-300 bg-opacity-40 border-gray-500 w-72 p-1 mx-auto rounded-lg mt-4 hover:scale-105 hover:border-neon transition"
           >
             <Image
               src={coach.image}
               alt={coach.name}
-              width={150}
+              width={250}
               height={150}
-              className="object-cover rounded-lg border border-gray-500"
+              className="object-cover rounded-lg border border-gray-500 drop-shadow-lg"
               priority
             />
-            <h1>{coach.name}</h1>
-            <h2>{coach.title}</h2>
+            <div className="flex flex-col items-center">
+              <h1 className="text-base font-medium">{coach.name}</h1>
+              <h2 className="text-sm">{coach.title}</h2>
+            </div>
           </Link>
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
-
-// import React from 'react'
-// import UnderConstruction from '../components/UnderConstruction'
-
-// const Coaches = () => {
-//   return (
-//     <div className='p-12'>
-//         <h1 className='text-white'>This is the Coaches Page</h1>
-//         <UnderConstruction />
-//     </div>
-//   )
-// }
-
-// export default Coaches
---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SocialIcons from './SocialIcons'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center py-4'>
+        <footer className='relative bottom-5'>
+            <SocialIcons />
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
