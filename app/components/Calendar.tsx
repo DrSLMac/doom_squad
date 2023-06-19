2ec797536b6794607d72f6914392f3cfcd20661f
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-06-19 17:17:43 -0500

    removed calendar until can be working properly

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
+import { DateSelectArg, EventClickArg, EventInput } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import { v4 as uuidv4 } from 'uuid';
 
-type Props = {
-  calendarData: string,
-  initialDate: string,
-}
+// type Props = {
+//   info: {
+//     start: string | undefined,
+//     end: string | undefined,
+//   },
+// }
 
-const Calendar = ( { calendarData, initialDate }: Props ) => {
+const Calendar = () => {
+    const [events, setEvents] = useState<EventInput[]>([])
     const calendarRef = useRef<FullCalendar | null>(null)
 
     useEffect(() => {
@@ -17,17 +22,47 @@ const Calendar = ( { calendarData, initialDate }: Props ) => {
         }
     }, [])
 
+    const handleSelect = (arg: DateSelectArg) => {
+        const { start, end } = arg;
+        const eventNamePrompt = prompt("Enter event name");
+        if (eventNamePrompt) {
+            const newEvent: EventInput = {
+                start,
+                end,
+                title: eventNamePrompt,
+                id: uuidv4(),
+                allDay: false,
+                color: 'blue'
+            };
+            setEvents([...events, newEvent]);
+        }
+    };
+
+    const handleEventClick = (arg: EventClickArg) => {
+        if (window.confirm("Are you sure you want to delete this event?")) {
+            const { id } = arg.event;
+            const updatedEvents = events.filter((event) => event.id !== id);
+            setEvents(updatedEvents);
+        }
+    };
+     
+
   return (
     <div className='h-[70%] p-8'>
         <FullCalendar 
             ref={calendarRef}
             plugins={[dayGridPlugin, interactionPlugin]}
-            headerToolbar={false}
+            headerToolbar={{
+                start: "today prev next",
+                end: "dayGridMonth dayGridWeek dayGridDay",
+            }}
             initialView='dayGridMonth'
-            initialDate={initialDate}
             contentHeight='600'
-            events={calendarData}
+            events={events}
+            select={handleSelect}
+            eventClick={handleEventClick}
             editable={true}
+            selectable={true}
             height='100%'
         />
     </div> 
